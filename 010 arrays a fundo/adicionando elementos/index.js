7708f61f1f5dec1c6dc1c26c69423140495f896a
Douglas S Werneck <swdoug_@hotmail.com>
2023-10-02 08:07:39 -0300

    feat: estudo

---
+++ 010 arrays a fundo/adicionando elementos/index.js
@@ -0,0 +1,20 @@
+/*
+    unshift = adiciona um item no inicio do array
+    push = adiciona um item no final do array
+
+*/
+
+let arr = [1, 2, 3, 4, 5];
+//exemplo basico onde seleciono o indice e coloco mais uma valor no array
+arr[5] = 6
+console.log(arr)
+
+arr.push(7)
+console.log(arr)
+//push adiciona no final do array
+
+
+arr.unshift(0)
+console.log(arr)
+//unshift adiciona no inicio do array
+
