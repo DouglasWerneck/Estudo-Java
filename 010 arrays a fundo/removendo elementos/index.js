fbe5a8b31a9bd75cd4cd50c456e0c5e39232dffc
Douglas S Werneck <swdoug_@hotmail.com>
2023-10-02 08:17:16 -0300

    estudo

---
+++ 010 arrays a fundo/removendo elementos/index.js
@@ -0,0 +1,14 @@
+/*
+    shift = remove o primeiro elemento de um array e retorna esse elemento
+    pop = remove o ultimo elemento de um array e retorna esse elemento
+
+*/
+
+let arr = [1, 2, 3, 4, 5];
+
+console.table(arr)
+console.log("shifit => " , arr.shift())
+console.log("pop => " ,arr.pop())
+
+
+console.table(arr)
